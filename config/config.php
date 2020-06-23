96dd5dce8d16884233a20dccd078751c0f4df6cf
Raquib <rkhaleeque1@jmi.ac.in>
2020-06-23 17:35:43 +0530

    Updated Code

--- config/config.php
+++ config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://10.68.1.122:8080/CI';
+$config['base_url'] = 'http://10.2.1.57:8080/CI';
 
 /*
 |--------------------------------------------------------------------------
